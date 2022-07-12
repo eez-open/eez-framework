23839e9ea1c7118a7cc947712b37a3045f260804
Martin Vladic <martin.vladic@gmail.com>
2022-07-12 17:24:49 +0200

    fix build

--- gui/keypad.cpp
+++ gui/keypad.cpp
@@ -16,6 +16,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <eez/conf.h>
+
 #if OPTION_KEYPAD
 
 #include <assert.h>
--- gui/touch_calibration.cpp
+++ gui/touch_calibration.cpp
@@ -16,6 +16,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <eez/conf.h>
+
 #if OPTION_TOUCH_CALIBRATION
 
 #include <eez/core/os.h>
