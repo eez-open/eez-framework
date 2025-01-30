b4f42bf8825f781873c71338a28722f165545500
Martin Vladic <martin.vladic@gmail.com>
2025-01-30 14:12:25 +0100

    do not add LVGL widgets to the execution queue

--- src/eez/flow/private.cpp
+++ src/eez/flow/private.cpp
@@ -88,6 +88,10 @@ bool isComponentReadyToRun(FlowState *flowState, unsigned componentIndex) {
         return false;
     }
 
+    if (component->type > defs_v3::FIRST_LVGL_WIDGET_COMPONENT_TYPE) {
+        return false;
+    }
+
     if ((component->type < defs_v3::COMPONENT_TYPE_START_ACTION && component->type != defs_v3::COMPONENT_TYPE_USER_WIDGET_WIDGET) || component->type >= defs_v3::FIRST_DASHBOARD_WIDGET_COMPONENT_TYPE) {
         // always execute widget
         return true;
